e6ac08d1d63f5902e987363480a8985f371cd474
Corwin Kerr <cbkerr@umich.edu>
2022-07-21 12:57:58 -0400

    Make project.py a template

--- 1-intro/project.py
+++ 1-intro/project.py
@@ -14,32 +14,11 @@ def points_generated(job):
     return "points" in job.data
 
 
-# So we know when to generate the overview plot
-@PiProject.label
-def all_calculated(*jobs):
-    return all(job.doc.get("pi_estimate", False) for job in jobs)
-
-
 @PiProject.operation
 @PiProject.post(points_generated) # the name of the label function
 def scatter_square(job):
-    """Generate job.sp.num_points number
-    of xy pairs of points within the [0,1] square 
-    and save it to the job data store.
-    
-    Optionally, initialize the random number
-    generator with the seed job.sp.seed"""
-    num_points = job.sp.num_points
-    
-    # Use the seed if our job has one
-    seed = job.sp.get("seed", None)
-    # We'll add seed to our state point later
-    rng = np.random.default_rng(seed)
-    
-    # defaults to within [0,1] in each dimension
-    points = rng.random(size=(num_points, 2))
-    job.data['points'] = points
-
+    # copy our code here
+    pass
 
 
 @PiProject.operation
@@ -47,51 +26,17 @@ def scatter_square(job):
 # use the 'truthiness' of dictionaries in Python:
 @PiProject.post.true('pi_estimated')
 def calculate_pi(job):
-    with job.data:
-        points = job.data.points[:]
-        selected = np.linalg.norm(points, axis=1) < 1
-        job.data['selected'] = selected
-        count = sum(selected)
-    num_points = job.statepoint.num_points
-    job.doc.pi_estimate = float(4 * count/num_points)
+    # copy our code here
+    pass
 
 
 @PiProject.operation
 @PiProject.pre.after(calculate_pi)
 @PiProject.post.isfile('preview.png')
 def render_image(job):
-    f,a = plt.subplots()
-    with job.data:
-        points = job.data.points[:]
-        selected = job.data.selected[:]
-    a.plot(*points.T, '.b')
-    a.plot(*selected.T, 'or')
-    a.set_aspect('equal')
-    plt.setp(a,
-             title = "Visualizing Pi Estimate",
-             )
-    plt.savefig(job.fn("preview.png"), format='png', dpi=150)
-    plt.close(f)
+    # copy our code here
+    pass
 
-@PiProject.operation
-@flow.aggregator(aggregator_function = None, sort_by="num_points")
-@PiProject.pre(all_calculated)
-def convergence_plot(*jobs):
-    # get data from each job in the aggregate
-    data = [job.doc.pi_estimate for job in jobs]
-    num_points = [job.sp.num_points for job in jobs]
-    
-    # get the project class so we know where to save our plot
-    project = jobs[0]._project
-    
-    # make the plot
-    f,a = plt.subplots()
-    a.plot(num_points, data)
-    plt.savefig(project.fn("convergence.png"), dpi=200)
-    plt.close()
-        
-    
-    
 
 if __name__ == '__main__':
     PiProject().main()
